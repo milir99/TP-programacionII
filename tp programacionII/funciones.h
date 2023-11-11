daa9f20528a4092439e9932f239dc949b44bc304
milir99 <milagrosabril.r@gmail.com>
2023-11-11 15:20:00 -0300

    archivos de funciones

--- tp programacionII/funciones.h
+++ tp programacionII/funciones.h
@@ -1,6 +1,23 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
+typedef struct nodoIngresos
+{
+int nroIngreso;
+char fechaIngreso[10];
+char fechaRetiro[10];
+int dniPaciente
+int matriculaProfesional;
+int eliminado;
+struct nodoIngresos*siguiente;
+struct nodoIngresos* anterior;
 
+}nodoIngresos
+typedef struct
+{
+     int nroPractica;
+    char nombrePractica[30];
+    int eliminado;
+} Practica;
 
 
 #endif // FUNCIONES_H_INCLUDED
